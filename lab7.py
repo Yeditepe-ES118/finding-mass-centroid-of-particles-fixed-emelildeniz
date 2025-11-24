5d54fe845c2b7192f2b6954e4e5dbfccdb935842
emelildeniz <emel.ildeniz@std.yeditepe.edu.tr>
2025-11-24 17:03:20 +0300

    Add files via upload

---
+++ lab7.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def centroid(p1x,p1y,p2x,p2y,p3x,p3y,m1,m2,m3):
+    positions = np.array([[p1x,p2x,p3x],
+                          [p1y,p2y,p3y]]) #2D
+                        
+    masses = np.array([m1,m2,m3]) #1D
+    
+    tot_mass =np.sum(masses)
+    
+    cx =np.sum(positions[0,:]* masses)/tot_mass
+    cy=np.sum(positions[1,:]*masses)/tot_mass
+    
+    return tot_mass,cx, cy,
+    
+
+centroid(1,1,0,1,1,0,3,4,100)
+
